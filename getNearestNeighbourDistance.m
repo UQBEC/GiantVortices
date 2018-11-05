fe85c1642f1280da4db3803434962b1f6b03808b
UQBEC <44692467+UQBEC@users.noreply.github.com>
2018-11-05 15:51:24 +1000

    Update getNearestNeighbourDistance.m

--- getNearestNeighbourDistance.m
+++ getNearestNeighbourDistance.m
@@ -1,7 +1,7 @@
 function ell = getNearestNeighbourDistance(x,y,N)
 Rij = zeros(N); %Matrix of distances
 for ii = 1:N
-    Rij(:,ii) = sqrt( (x-x(ii)).^2 + (y-y(ii).^2) );
+    Rij(:,ii) = sqrt( (x-x(ii)).^2 + (y-y(ii)).^2 );
     Rij(ii,ii) = NaN; %exclude diagonal terms
 end
 ell = mean(nanmin(Rij));
